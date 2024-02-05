4c454a7381ac2befefffaf28bfda95f0c2f22ff0
by_Onyx <ilyayaldinov@gmail.com>
2024-02-05 21:09:55 +0300

    change GetAllSubgroupProductsHandler

--- Handlers/SubgroupHandlers/GetAllSubgroupProductsHandler.cs
+++ Handlers/SubgroupHandlers/GetAllSubgroupProductsHandler.cs
@@ -11,28 +11,41 @@ public class GetAllSubgroupProductsHandler : IRequestHandler<GetAllSubgroupProdu
 {
     private readonly IProductService _productService;
     private readonly IPhotoService _photoService;
+    private readonly IManufacturerService _manufacturerService;
     private readonly IMapper _mapper;
 
-    public GetAllSubgroupProductsHandler(IProductService productService, IPhotoService photoService, IMapper mapper)
+    public GetAllSubgroupProductsHandler(IProductService productService, IPhotoService photoService, IMapper mapper, IManufacturerService manufacturerService)
     {
         _productService = productService;
         _photoService = photoService;
         _mapper = mapper;
+        _manufacturerService = manufacturerService;
     }
 
     public async Task<GetAllSubgroupProductsResponse> Handle(GetAllSubgroupProductsRequest request, CancellationToken cancellationToken)
     {
         var products = await _productService.GetAllProductsBySubgroupId(request.SubgroupId);
-        var productCards = products.Select(_mapper.Map<ProductCardModel>).ToArray();
+        var productCards = products.Select(_mapper.Map<ProductCardModel>).GroupBy(x => x.ManufacturerId).ToList();
+
+        List<ManufacturerGroupModel> models = new List<ManufacturerGroupModel>(); 
         
-        foreach (var product in productCards)
+        foreach (var product in productCards.SelectMany(x => x))
         {
             product.Photos = _mapper.Map<PhotoForCardModel[]>(await _photoService.GetAllPhotosByProductIdAsync(product.Id));
         }
 
+        foreach (var productCard in productCards)
+        {
+            models.Add(new ManufacturerGroupModel
+            {
+                Manufacturer = await _manufacturerService.GetManufacturerByIdAsync(productCard.Key),
+                Products = productCards.Where(x => x.Key == productCard.Key).SelectMany(x => x).ToArray()
+            });
+        }
+        
         var response = new GetAllSubgroupProductsResponse
         {
-            Products = productCards
+            ManufacturerGroupModels = models.ToArray()
         };
 
         return response;
--- Responses/SubgroupResponses/GetAllSubgroupProductsResponse.cs
+++ Responses/SubgroupResponses/GetAllSubgroupProductsResponse.cs
@@ -1,9 +1,16 @@
-﻿using net_shop_back.Models;
+﻿using net_shop_back.Entities;
+using net_shop_back.Models;
 
 namespace net_shop_back.Responses.SubgroupResponses;
 
 public record GetAllSubgroupProductsResponse
 {
+    public required ManufacturerGroupModel[] ManufacturerGroupModels { get; set; }
+}
+
+public record ManufacturerGroupModel
+{
+    public required Manufacturer? Manufacturer { get; set; }
     public required ProductCardModel[] Products { get; set; }
     public int Count => Products.Length;
 }
